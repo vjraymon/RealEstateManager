a55d69b3293c06c5a871ef89a2b19377b44ec980
vjraymon <vjraymon@gmail.com>
2022-03-13 05:26:01 +0100

    Format the prices in $

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/DisplayDetailedPropertyFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/DisplayDetailedPropertyFragment.java
@@ -27,6 +27,7 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
